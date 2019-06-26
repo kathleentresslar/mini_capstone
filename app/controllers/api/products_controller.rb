c38e9afb9c2bbc4b421cafca13b3948ead8b2e0b
KathleenTresslar <kathleen.tresslar@gmail.com>
2019-06-25 19:04:14 -0500

    Adding routes for just showing one product for first two products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,14 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "all_products.json.jb"
   end
+
+  def show_brownie
+    @brownie = Product.find_by(name: "brownie")
+    render "show_brownie.json.jb"
+  end
+
+  def show_cheesecake
+    @cheesecake = Product.find_by(name: "cheesecake")
+    render "show_cheesecake.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/all_products" => "products#show_all_products"
+    get "/brownie" => "products#show_brownie"
+    get "/cheesecake" => "products#show_cheesecake"
   end
 end
